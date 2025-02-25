0bf4f9a73fc71450bc30a723f21cd951e17bbbb6
alessioromita <romitaalessio@gmail.com>
2025-02-25 16:37:54 +0100

    Logic insert queue
    Added logic to when insert the study_uid into the queue. Before inserting the uid in the queue we check if in the table the uid is already present

--- dicomsorter/DicomStoreHandler.py
+++ dicomsorter/DicomStoreHandler.py
@@ -1,13 +1,15 @@
 import os
 import logging
-from .global_variables import BASE_DIR
+from .src.global_var import BASE_DIR
 from pynetdicom import AE
 import uuid
 from datetime import datetime
-from .query import INSERT_QUERY_DICOM_META, INSERT_QUERY_DICOM_ASS
-from .src.dicom_data import return_dicom_data
+from .query import INSERT_QUERY_DICOM_META, INSERT_QUERY_DICOM_ASS, \
+    UNIQUE_UID_SELECT
+from .src.dicom_data import return_dicom_data, create_folder
 from .src.global_var import SCP_AE_TITLE, QUEUE_NAME, RABBITMQ_URL
 import pika
+
 
 
 class DicomStoreHandler:
@@ -21,16 +23,44 @@ class DicomStoreHandler:
 
     def open_connection(self):
         """Establish connection"""
+
         connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_URL))
         self.connection = connection
 
+
     def close_connection(self):
         """Close connection"""
+
         self.connection.close()
 
     def create_queue(self):
         channel = self.connection.channel()
-        channel.queue_declare(queue=QUEUE_NAME, passive=True)
+        channel.queue_declare(queue=QUEUE_NAME, passive=False, durable=True)
+
+    def send_to_queue(self, message):
+        # Convert Python dict to JSON string
+        self.connection.channel().basic_publish(
+            exchange='',
+            routing_key=QUEUE_NAME,
+            body=message.encode('utf-8'),  # Convert to bytes
+            properties=pika.BasicProperties(delivery_mode=2)  # Persistent messages
+        )
+
+    def check_uid_db(self, study_uid):
+        """
+
+        :param study_uid:
+        :return:
+        """
+        result = self.db.fetch_all(UNIQUE_UID_SELECT, params=study_uid)
+        if not result:
+            try:
+                self.send_to_queue(study_uid)
+                logging.info(f"Inserting {study_uid} in the queue")
+            except:
+                logging.warning("Inserting in the queue failed.")
+                raise
+        logging.info(f"Insertion complete")
 
     def handle_assoc_open(self, event):
         """
@@ -59,18 +89,15 @@ class DicomStoreHandler:
         ds = event.dataset
         ds.file_meta = event.file_meta
         assoc_id = event.assoc.assoc_id
+
         patient_id, study_uid, series_uid, modality, sop_uid, sop_class_uid, \
             instance_number, modality_type = return_dicom_data(ds)
 
-        # Create directories for storage
-        patient_folder = os.path.join(BASE_DIR, patient_id, study_uid, modality)
-        os.makedirs(patient_folder, exist_ok=True)
-
-        # Save the DICOM file
-        filename = os.path.join(patient_folder, f"{sop_uid}.dcm")
+        filename = create_folder(patient_id, study_uid, modality, sop_uid)
+        logging.info(f"Folder structure create. Saving in {filename}")
         ds.save_as(filename, write_like_original=False)
 
-        logger.info(f"[INFO] Stored {modality} file for Patient {patient_id}: {filename}")
+        logging.info(f"[INFO] Stored {modality} file for Patient {patient_id}: {filename}")
         params = (
             patient_id,
             study_uid,
@@ -83,7 +110,21 @@ class DicomStoreHandler:
             modality_type,
             assoc_id
         )
-        logging.info(f"Inserting value in Meta table")
+        logging.info(f"Checking if {study_uid} in database before inserting into the queue and table")
+
+        result = self.db.fetch_all(UNIQUE_UID_SELECT, params=study_uid)
+        if not result:
+            try:
+                self.send_to_queue(study_uid)
+                logging.info(f"Inserting {study_uid} in the queue")
+            except:
+                logging.warning("Inserting in the queue failed.")
+                raise
+        logging.info(f"Insertion complete")
+        self.check_uid_db(study_uid)
+
+        logging.info("Inserting dicom metadata into the table")
         self.db.execute_query(INSERT_QUERY_DICOM_META, params)
+        logging.info("Insert complete")
 
         return 0x0000

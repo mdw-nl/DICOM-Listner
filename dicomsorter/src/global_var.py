e28449666a63c8355f99c2858a8c874776e45d58
TomSinsel <sinseltom@gmail.com>
2025-10-17 14:26:13 +0200

    Changed the username and password to guest/guest for testing

--- dicomsorter/src/global_var.py
+++ dicomsorter/src/global_var.py
@@ -1,6 +1,6 @@
 SCP_AE_TITLE = "MY_SCP"
 NUMBER_ATTEMPTS = 5
 RETRY_DELAY_IN_SECONDS = 10
-RABBITMQ_URL = "amqp://user:password@rabbitmq:5672/"
+RABBITMQ_URL = "amqp://guest:guest@rabbitmq:5672/"
 QUEUE_NAME = "DICOM_Processor"
 BASE_DIR = "./data/"
